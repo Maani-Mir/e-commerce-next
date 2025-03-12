8f18c28ee4ee0966f6dae3e24e75f5ef52a204f0
Salman Arif <salmanarifmir2000@gmail.com>
2025-03-12 12:11:03 +0500

    Update alignment of Explore items

--- src/components/Explore/explore-item.js
+++ src/components/Explore/explore-item.js
@@ -6,13 +6,13 @@ export default function ExploreItem({ title, image }) {
   // console.log("the image in item", title);
   // console.log("the title in item", image);
   return (
-    <div className="flex-grow-0 flex-shrink-0 basis-auto bg-[#1c1816] p-2 mt-5 box-border text-center rounded-l-full rounded-tr-full rounded-br-3xl w-60 h-56 transition duration-300 ease-in-out hover:-translate-y-4 ">
+    <div className="flex flex-col items-center justify-center flex-grow-0 flex-shrink-0 basis-auto  bg-[#1c1816] p-2 mt-5 box-border text-center rounded-l-full rounded-tr-full rounded-br-3xl w-60 h-56 transition duration-300 ease-in-out hover:-translate-y-4 ">
       <Image
         src={image}
         alt="explore-menu-image"
         width={200}
         height="auto"
-        className="m-auto"
+        // className="m-auto"
       />
       <div className="font-semibold text-nowrap text-base tracking-tight ">
         {title}
