51b6903ad4f9d7f0ce23778b0281c9e5d857878f
Salman Arif <salmanarifmir2000@gmail.com>
2025-03-14 08:02:10 +0500

    Updated image slider to its own component
    To avoid using "use client" on the app/index file

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,20 @@
-"use client";
-
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import SLIDER_IMAGES from "../constants/image-sliders";
-import Slider from "@madzadev/image-slider";
 import Explore from "@/components/Explore";
-import "@madzadev/image-slider/dist/index.css";
 import Link from "next/link";
 import Sellers from "@/components/Sellers";
 import Deals from "@/components/Deals";
 import Hero from "@/components/Hero";
+import ImageSlider from "@/components/ImageSlider";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <main>
-        <div className="mt-24">
-          <Slider
-            imageList={SLIDER_IMAGES}
-            width={1915}
-            height={719}
-            showDotControls={false}
-          />
-        </div>
+        <ImageSlider />
         <div className="flex items-center justify-center mt-4">
           <Link
             href="#"
--- src/components/ImageSlider/index.js
+++ src/components/ImageSlider/index.js
@@ -1,160 +1,19 @@
-// "use client";
+"use client";
 
-// import React, { useEffect, useState } from "react";
-// import Image1 from "../../../public/1.jpg";
-// import Image2 from "../../../public/2.jpg";
-// import Image3 from "../../../public/3.jpg";
-// import Image4 from "../../../public/4.jpg";
-// import Image from "next/image";
+import React from "react";
+import Slider from "@madzadev/image-slider";
+import "@madzadev/image-slider/dist/index.css";
+import SLIDER_IMAGES from "../../constants/image-sliders";
 
-// const images = [
-//   {
-//     imgURL: Image1,
-//     imgAlt: "img-1",
-//   },
-//   {
-//     imgURL: Image2,
-//     imgAlt: "img-2",
-//   },
-//   {
-//     imgURL: Image3,
-//     imgAlt: "img-3",
-//   },
-//   {
-//     imgURL: Image4,
-//     imgAlt: "img-4",
-//   },
-// ];
-
-// export default function ImageSlider() {
-//   const [activeIndex, setActiveIndex] = useState(0);
-//   const [isHovered, setIsHovered] = useState(false);
-
-//   // to handle automatic slide transition
-//   useEffect(() => {
-//     if (!isHovered) {
-//       // if not hovered, start interval for automatic slide change
-//       const interval = setInterval(() => {
-//         slideNext();
-//       }, 3000);
-
-//       // when unmounts, clear the interval
-//       return () => {
-//         clearInterval(interval);
-//       };
-//     }
-//   }, [isHovered]);
-
-//   // const slideNext = () => {
-//   //   setActiveIndex((val) => {
-//   //     if (val >= children.length - 1) {
-//   //       return 0;
-//   //     } else {
-//   //       return val + 1;
-//   //     }
-//   //   });
-//   // };
-
-//   // const slideNext = () => {
-//   //   setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-//   // };
-
-//   const slideNext = () => {
-//     setActiveIndex((prevIndex) => {
-//       if (prevIndex >= images.length - 1) {
-//         return 0;
-//       } else {
-//         return prevIndex + 1;
-//       }
-//     });
-//   };
-
-//   const slidePrev = () => {
-//     setActiveIndex((prevIndex) => {
-//       if (prevIndex <= 0) {
-//         return images.length - 1;
-//       } else {
-//         return prevIndex - 1;
-//       }
-//     });
-//   };
-
-//   // const slidePrev = () => {
-//   //   setActiveIndex(
-//   //     (prevIndex) => (prevIndex - 1 + images.length) % images.length
-//   //   );
-//   // };
-
-//   // const slidePrev = () => {
-//   //   setActiveIndex((val) => {
-//   //     if (val <= 0) {
-//   //       return children.length - 1;
-//   //     } else {
-//   //       return val - 1;
-//   //     }
-//   //   });
-//   // };
-
-//   const handleMouseOver = () => {
-//     setIsHovered(true);
-//   };
-
-//   const handleMouseLeave = () => {
-//     setIsHovered(false);
-//   };
-
-//   return (
-//     <div>
-//       <div
-//         className="container__slider"
-//         onMouseOver={handleMouseOver}
-//         onMouseLeave={handleMouseLeave}
-//       >
-//         <Image
-//           src={images[activeIndex].imgURL}
-//           alt={images[activeIndex].imgAlt}
-//           width="0"
-//           height="0"
-//           layout="responsive"
-//           style={{ width: "100%", height: "auto" }}
-//         />
-//         {/* {children.map((item, index) => {
-//         return (
-//           <div
-//             className={"slider__item slider__item-active-" + (activeIndex + 1)}
-//             key={index}
-//           >
-//             {item}
-//           </div>
-//         );
-//       })}
-
-//       <div className="container__slider__links">
-//         {children.map((item, index) => {
-//           return (
-//             <button
-//               key={index}
-//               className={
-//                 activeIndex === index
-//                   ? "container__slider__links-small container__slider__links-small-active"
-//                   : "container__slider__links-small"
-//               }
-//               onClick={(e) => {
-//                 e.preventDefault();
-//                 setActiveIndex(index);
-//               }}
-//             ></button>
-//           );
-//         })}
-//       </div> */}
-
-//         <button className="slider__btn-next" onClick={slideNext()}>
-//           {">"}
-//         </button>
-//         <button className="slider__btn-prev" onClick={slidePrev()}>
-//           {"<"}
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
+export default function ImageSlider() {
+  return (
+    <div className="mt-24">
+      <Slider
+        imageList={SLIDER_IMAGES}
+        width={1915}
+        height={719}
+        showDotControls={false}
+      />
+    </div>
+  );
+}
