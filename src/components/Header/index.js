34ae816c799e3ad14b04d48ae63b2500bf3de49d
Salman Arif <salmanarifmir2000@gmail.com>
2025-03-17 09:43:37 +0500

    Update Header and Hero

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,21 +8,22 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <nav className="fixed z-10 top-0 bg-[#0A0A0A]">
+    <header className="">
+      <nav className="fixed z-10 top-0 bg-[#0A0A0A] mx-auto items-center justify-between">
         <div className="flex flex-row">
           {/* <nav> */}
-          <div className={styles.header__logo}>
+          <div className="">
             <Image
               src={logoImage}
               alt="kfc-logo"
               width={100}
               height="auto"
               placeholder="blur"
+              className="mt-10 ml-8 md:ml-24 lg:ml-48 mb-7"
             />
           </div>
-          <div className="flex flex-row">
-            <div className="pl-5 pt-11">
+          <div className="flex flex-row ml-40 mt-16 lg:mt-11">
+            <div className="">
               <Link
                 href="#"
                 className="bg-[#1C1816] p-2.5 px-8 rounded-lg font-bold tracking-wide text-sm border-2 border-[#EA002A]"
@@ -36,7 +37,7 @@ export default function Header() {
                 DELIVERY
               </Link>
             </div>
-            <div className="pl-4 pt-11">
+            <div className="">
               <Link
                 href="#"
                 className="bg-[#1C1816] ml-5 p-2.5 px-8 rounded-lg font-bold tracking-wide text-sm"
@@ -51,10 +52,10 @@ export default function Header() {
               </Link>
             </div>
           </div>
-          <div className="ml-[800px] justify-end pt-11 pr-80">
+          <div className="sm:ml-[100px] sm:mr-6 md:ml-[155px] md:mr-40 lg:ml-[250px] xl:ml-[720px] mt-11 2xl:mr-96">
             <Link
               href="#"
-              className="bg-[#EA002A] p-2.5 rounded-lg font-bold px-3.5 text-sm"
+              className="bg-[#EA002A] p-2.5 rounded-lg font-bold px-3.5 text-sm "
             >
               LOGIN
             </Link>
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,61 +8,59 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <>
-      <div className="flex flex-row mt-20">
-        <div>
-          <Image
-            className=" rounded-2xl"
-            src={Hero1}
-            alt="hero-menu-image"
-            width={662}
-            height="auto"
-          />
-        </div>
-        <div className="ml-5">
-          <Image
-            className="rounded-2xl"
-            src={Hero2}
-            alt="hero-menu-image"
-            width={662}
-            height="auto"
-          />
+    <div className=" grid mt-20 grid-cols-1 md:grid-cols-2 gap-4 ">
+      {/* <div className=""> */}
+      <div className="">
+        <Image
+          className=" rounded-2xl"
+          src={Hero1}
+          alt="hero-menu-image"
+          width={650}
+          // height="auto"
+        />
+      </div>
+      <div className="">
+        <Image
+          className="  rounded-2xl"
+          src={Hero2}
+          alt="hero-menu-image"
+          width={650}
+          height="auto"
+        />
+      </div>
+      <div className="">
+        <Image
+          className="  rounded-2xl"
+          src={Hero3}
+          alt="hero-menu-image"
+          width={650}
+          height="auto"
+        />
+      </div>
+      <div className="flex flex-col items-center bg-[#1C1816] rounded-2xl  ">
+        <Image
+          className="shrink"
+          src={Hero4}
+          alt="hero-menu-image"
+          width={260}
+          height="auto"
+        />
+        <p className="pl-6 pt-5 text-xl pr-6 font-bold tracking-wide text-center">
+          Adding 11 herbs and spices, Explore our menu and add items to your
+          cart.
+        </p>
+        {/* <p className="text-xl text-center font-bold tracking-wide">
+            
+          </p> */}
+        <div className="shrink text-center pt-7 ">
+          <Link
+            href="#"
+            className="bg-[#EA002A] p-3 rounded-lg font-bold px-10 text-xl"
+          >
+            EXPLORE MENU
+          </Link>
         </div>
       </div>
-      <div className="flex flex-row mt-8">
-        <div>
-          <Image
-            className=" rounded-2xl"
-            src={Hero3}
-            alt="hero-menu-image"
-            width={662}
-            height="auto"
-          />
-        </div>
-        <div className="ml-5 bg-[#1C1816] rounded-2xl ">
-          <Image
-            className="ml-48 mt-4"
-            src={Hero4}
-            alt="hero-menu-image"
-            width={260}
-            height="auto"
-          />
-          <p className="pl-6 pt-5 text-xl pr-6 font-bold tracking-wide">
-            Adding 11 herbs and spices, Explore our menu and add items
-          </p>
-          <p className="text-xl text-center font-bold tracking-wide">
-            to your cart.
-          </p>
-          <div className="text-center pt-7 ">
-            <Link
-              href="#"
-              className="bg-[#EA002A] p-3 rounded-lg font-bold px-10 text-xl"
-            >
-              EXPLORE MENU
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
